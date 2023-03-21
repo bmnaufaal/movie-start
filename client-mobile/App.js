45752e15c63a2efbbe8ebc06094ed6e1fd3f65e1
bmnaufaal <bmuhammadopang@gmail.com>
2023-03-21 16:07:57 +0700

    feat: fetch movies data

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,23 +1,75 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import MainStack from "./navigators/MainStack";
 import { NavigationContainer } from "@react-navigation/native";
 import Tab from "./navigators/Tab";
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
+  const fontConfig = {
+    web: {
+      regular: {
+        fontFamily: "sans-serif",
+        fontWeight: "normal",
+      },
+      medium: {
+        fontFamily: "sans-serif-medium",
+        fontWeight: "normal",
+      },
+      light: {
+        fontFamily: "sans-serif-light",
+        fontWeight: "normal",
+      },
+      thin: {
+        fontFamily: "sans-serif-thin",
+        fontWeight: "normal",
+      },
+    },
+    ios: {
+      regular: {
+        fontFamily: "sans-serif",
+        fontWeight: "normal",
+      },
+      medium: {
+        fontFamily: "sans-serif-medium",
+        fontWeight: "normal",
+      },
+      light: {
+        fontFamily: "sans-serif-light",
+        fontWeight: "normal",
+      },
+      thin: {
+        fontFamily: "sans-serif-thin",
+        fontWeight: "normal",
+      },
+    },
+    android: {
+      regular: {
+        fontFamily: "sans-serif",
+        fontWeight: "normal",
+      },
+      medium: {
+        fontFamily: "sans-serif-medium",
+        fontWeight: "normal",
+      },
+      light: {
+        fontFamily: "sans-serif-light",
+        fontWeight: "normal",
+      },
+      thin: {
+        fontFamily: "sans-serif-thin",
+        fontWeight: "normal",
+      },
+    },
+  };
+
+  const theme = {
+    ...DefaultTheme,
+    fonts: configureFonts(fontConfig),
+  };
   return (
-    <NavigationContainer>
-      {/* <MainStack /> */}
-      <Tab />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Tab />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- client-mobile/screens/Home.js
+++ client-mobile/screens/Home.js
@@ -1,17 +1,44 @@
-import { View, Text, Button } from "react-native";
+import { useState } from "react";
+import { View, Text, Button, ScrollView } from "react-native";
+import { Card, Title, Paragraph } from "react-native-paper";
 
 export default function Home({ navigation }) {
+  const [movies, setMovies] = useState([]);
+  const fetchMoviesData = async () => {
+    fetch("https://api.pilem-start.shop/movies")
+      .then(async (res) => {
+        if (!res.ok) throw await res.text();
+        return res.json();
+      })
+      .then((data) => setMovies(data))
+      .catch((error) => console.log(error));
+  };
+
+  useState(() => {
+    fetchMoviesData();
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Detail"
-        onPress={() => {
-          navigation.navigate("Detail", {
-            id: 1
-          });
-        }}
-      />
+    <View>
+      <ScrollView>
+        {movies?.map((movie) => {
+          return (
+            <Card
+              onPress={() => {
+                navigation.navigate("Detail", {
+                  id: movie.id,
+                });
+              }}
+            >
+              <Card.Content>
+                <Card.Cover source={{ uri: movie.imgUrl }} />
+                <Title>{movie.title}</Title>
+                <Paragraph>{movie.Genre.name}</Paragraph>
+              </Card.Content>
+            </Card>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
