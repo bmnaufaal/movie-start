b2a429b06ef37a69b3ab298afbb70d3ac1839134
bmnaufaal <bmuhammadopang@gmail.com>
2023-03-23 14:44:20 +0700

    feat: mongodb user done

--- server/services/users/controllers/userController.js
+++ server/services/users/controllers/userController.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const { ObjectId } = require("mongodb");
-const { mongoConnect, getDatabase } = require("../config/mongoConnection");
+const { getDatabase } = require("../config/mongoConnection");
+const { hashPassword } = require("../helpers/bcrypt");
 
 class UserController {
   static async findAll(req, res, next) {
@@ -9,6 +10,10 @@ class UserController {
       const db = getDatabase();
       const usersCollection = db.collection("users");
       const users = await usersCollection.find({}).toArray();
+      users.map((user) => {
+        delete user.password;
+        return user;
+      });
       res.status(200).json({
         message: "Find All Users",
         data: users,
@@ -26,10 +31,55 @@ class UserController {
       const { id } = req.params;
       const db = getDatabase();
       const usersCollection = db.collection("users");
-      const users = await usersCollection.findOne({ _id: new ObjectId(id) });
+      let user = await usersCollection.findOne({ _id: new ObjectId(id) });
       res.status(200).json({
         message: "Find Users By Id",
-        data: users,
+        data: user,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  }
+
+  static async create(req, res, next) {
+    try {
+      const { username, email, password, phoneNumber, address } = req.body;
+      const db = getDatabase();
+      const usersCollection = db.collection("users");
+      const users = await usersCollection.find({}).toArray();
+      await usersCollection.insertOne({
+        id: users[users.length - 1].id + 1,
+        username: username,
+        email: email,
+        password: hashPassword(password),
+        role: "admin",
+        phoneNumber: phoneNumber,
+        address: address,
+      });
+      res.status(201).json({
+        message: "Success created user ",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  }
+
+  static async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+      const db = getDatabase();
+      const usersCollection = db.collection("users");
+      await usersCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.status(200).json({
+        message: "Success delete user ",
       });
     } catch (error) {
       console.log(error);
--- server/services/users/routers/user.js
+++ server/services/users/routers/user.js
@@ -2,6 +2,8 @@ const UserController = require("../controllers/userController");
 
 const userRouter = require("express").Router();
 
+userRouter.post("/users/create", UserController.create);
+userRouter.delete("/users/:id", UserController.delete);
 userRouter.get("/users", UserController.findAll);
 userRouter.get("/users/:id", UserController.findOne);
 
