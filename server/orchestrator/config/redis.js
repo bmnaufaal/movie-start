fc9f54f13c0aba6dc014ec78e4264df029bc8820
bmnaufaal <bmuhammadopang@gmail.com>
2023-03-28 18:49:42 +0700

    fix: ioredis

--- server/orchestrator/config/redis.js
+++ server/orchestrator/config/redis.js
@@ -3,10 +3,15 @@ const Redis = require("ioredis");
 // Create a Redis instance.
 // By default, it will connect to localhost:6379.
 // We are going to cover how to specify connection options soon.
+
 const redis = new Redis({
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOST_URL,
   password: process.env.REDIS_PASSWORD,
 });
+
+console.log(process.env.PORT_REDIS);
+console.log(process.env.HOST_REDIS);
+console.log(process.env.PASS_REDIS);
 
 module.exports = redis;
